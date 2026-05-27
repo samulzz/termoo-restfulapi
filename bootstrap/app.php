6cb470da488e74877a6a8bf401fa9942f9ec3197
Samuel Araujo Brota <samuel.araujo.130823@gmail.com>
2026-05-27 17:12:39 -0300

    inicio

---
+++ bootstrap/app.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        api: __DIR__.'/../routes/api.php',
+        apiPrefix: 'api',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->render(function (NotFoundHttpException $exception, Request $request) {
+            return response()->json(['erro' => 'Jogo não encontrado ou rota inexistente.'], 404);
+        });
+
+        $exceptions->shouldRenderJsonWhen(fn (Request $request, Throwable $e) => true);
+    })
+    ->create();
